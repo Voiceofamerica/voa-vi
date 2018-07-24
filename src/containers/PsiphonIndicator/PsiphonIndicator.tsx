dd58809cdbab82ffd2287be99e07f9931a7d59d8
Kevin Groat <apps@kevingroat.io>
2018-07-24 13:17:13 -0400

    Fixed psiphon indicator

--- src/containers/PsiphonIndicator/PsiphonIndicator.tsx
+++ src/containers/PsiphonIndicator/PsiphonIndicator.tsx
@@ -1,22 +1,53 @@
 
 import * as React from 'react'
+import { Subscription } from 'rxjs/Subscription'
 import { connect, Dispatch } from 'react-redux'
 
-import { psiphonIndicator } from './PsiphonIndicator.scss'
+import { startObservable } from '@voiceofamerica/voa-shared/helpers/psiphonHelper'
 import toggleMediaDrawer from 'redux-store/actions/toggleMediaDrawer'
 import toggleCircumventionDrawer from 'redux-store/actions/toggleCircumventionDrawer'
+
+import { psiphonIndicator } from './PsiphonIndicator.scss'
+
+const PSIPHON_ON = require('./psiphon_on.svg')
+const PSIPHON_OFF = require('./psiphon_off.svg')
 
 interface DispatchProps {
   toggleCircumventionDrawer: () => void
 }
 
-interface Props extends React.Props<HTMLImageElement>, DispatchProps {
+interface Props extends DispatchProps {
 }
 
-const PsiphonIndicatorBase = ({ ref, toggleCircumventionDrawer }: Props) => {
-  return (
-    <img ref={ref} className={psiphonIndicator} src={require('./psiphon_on.svg')} onClick={toggleCircumventionDrawer} />
-  )
+interface State {
+  psiphonOn: boolean
+}
+
+class PsiphonIndicatorBase extends React.PureComponent<Props, State> {
+  state: State = {
+    psiphonOn: startObservable.getValue(),
+  }
+
+  private subscription: Subscription
+
+  componentDidMount () {
+    this.subscription = startObservable.subscribe(psiphonOn => this.setState({ psiphonOn }))
+  }
+
+  componentWillUnmount () {
+    this.subscription.unsubscribe()
+  }
+
+  render () {
+    const { psiphonOn } = this.state
+    const { toggleCircumventionDrawer } = this.props
+
+    const srcToUse = psiphonOn ? PSIPHON_ON : PSIPHON_OFF
+
+    return (
+      <img className={psiphonIndicator} src={srcToUse} onClick={toggleCircumventionDrawer} />
+    )
+  }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch<any>): DispatchProps => ({
